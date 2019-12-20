bf4daedb03582c433fa3b4e61e634b5140626dd5
Scott Farley <scott.farley@mapbox.com>
2019-12-20 14:58:34 -0600

    lint

--- docs/pages/example/mapbox-gl-geocoder-no-map.js
+++ docs/pages/example/mapbox-gl-geocoder-no-map.js
@@ -9,4 +9,5 @@ pathname: /mapbox-gl-js/example/mapbox-gl-geocoder-no-map/
 ---*/
 import Example from '../../components/example';
 import html from './mapbox-gl-geocoder-no-map.html';
+
 export default Example(html);
